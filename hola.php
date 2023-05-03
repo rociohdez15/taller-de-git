05e760bc5a96b7afdea8c958b1016e427ca49e35
rociohdez15 <rociohdez2706@hotmail.com>
2023-05-03 19:14:00 +0000

    Programa interactivo.-Rocio

--- hola.php
+++ hola.php
@@ -1,6 +1,6 @@
 <?php
 echo "Rocio Hernandez Silva\n";
 // Autora: Rocio Hernandez Silva
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
