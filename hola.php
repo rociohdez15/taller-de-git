d7b53b4496a2aaca0c21f1464abd18f4af3de699
rociohdez15 <rociohdez2706@hotmail.com>
2023-05-03 18:32:54 +0000

    Ups... este commit está mal.-Rocio

--- hola.php
+++ hola.php
@@ -1,5 +1,5 @@
 <?php
 echo "Rocio Hernandez Silva\n";
-// El nombre por defecto es Mundo
+// Este comentario está mal y hay que borrarlo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
