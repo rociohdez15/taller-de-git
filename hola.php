50edbbb56e5ed850b21f656362974b6c56583ba7
rociohdez15 <rociohdez2706@hotmail.com>
2023-05-03 18:39:15 +0000

    Añadida la autora del programa y sus apellidos

--- hola.php
+++ hola.php
@@ -1,5 +1,6 @@
 <?php
 echo "Rocio Hernandez Silva\n";
+// Autora: Rocio Hernandez Silva
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
